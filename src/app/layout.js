b34f50f65dc0c505e06dcd2d91fde187bf89dbd0
santiago maza <mazasantiago.10@gmail.com>
2023-06-08 18:55:36 -0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
+"use client"
+
+import Navbar from './components/navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +15,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        
+        <Navbar/>
+
+        {children}
+        
+        
+        </body>
     </html>
   )
 }
